47e705b2c53079e650cacbab95a702738ce340c1
jenil <jenilsakriya612@gmail.com>
2025-06-08 18:41:16 +0530

    Profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import Post from '../components/Post';
+import Post from '../components/postFormation/Post';
 import { useTheme } from '../context/ThemeContext';
 import api from '../utils/api';
 import { CameraIcon } from '@heroicons/react/24/outline';
