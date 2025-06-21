08c52d8565cc69fb38d5be75cd1f3ce17be96f9f
jenil <jenilsakriya612@gmail.com>
2025-06-21 14:07:49 +0530

    updated post button

--- frontend/src/pages/posts/CreatePost.jsx
+++ frontend/src/pages/posts/CreatePost.jsx
@@ -359,7 +359,7 @@ function CreatePost({ isOpen, onClose, onPostCreated }) {
                 key={category._id}
                 type="button"
                 onClick={() => handleCategoryToggle(category._id)}
-                className={`px-3 py-1 rounded-full text-sm ${categoryIds.includes(category._id) ? 'bg-blue-500 text-white' : 'bg-gray-200 text-theme hover:bg-gray-300'}`}
+                className={`px-3 py-1 rounded-full text-sm ${categoryIds.includes(category._id) ? 'bg-blue-500 text-white' : 'background text-theme hover:bg-gray-300'}`}
               >
                 {category.name}
               </button>
@@ -404,12 +404,12 @@ function CreatePost({ isOpen, onClose, onPostCreated }) {
           >
             Cancel
           </button>
-          <button
-            onClick={handleSubmit}
-            className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-            disabled={loading}
-          >
-            {loading ? 'Posting...' : 'Post'}
+          <button onClick={handleSubmit} class="relative inline-flex items-center justify-start px-6 py-3 overflow-hidden font-medium transition-all bg-blue-500 rounded-xl group">
+            <span class="absolute top-0 right-0 inline-block w-4 h-4 transition-all duration-500 ease-in-out bg-blue-700 rounded group-hover:-mr-4 group-hover:-mt-4">
+              <span class="absolute top-0 right-0 w-5 h-5 rotate-45 translate-x-1/2 -translate-y-1/2 bg-white"></span>
+            </span>
+            <span class="absolute bottom-0 left-0 w-full h-full transition-all duration-500 ease-in-out delay-200 -translate-x-full translate-y-full bg-blue-600 rounded-2xl group-hover:mb-12 group-hover:translate-x-0"></span>
+            <span class="relative w-full text-left text-white transition-colors duration-200 ease-in-out group-hover:text-white">{loading ? 'Posting...' : 'Post'}</span>
           </button>
         </div>
       </div>
